f036ea879d2129fb284f4ca843b588302b02b4fa
Mohamed BEN AYED <mohamed.benayed@ext.leroymerlin.fr>
2018-02-12 19:57:24 +0100

    change

--- newjs.js
+++ newjs.js
@@ -10,36 +10,116 @@ jQuery.extend(jQuery.expr[':'], {
     }
 });
 $(function(){
+	
+	$.ajaxSetup({async: false});
 	var vars = {};
 	$("newjs").each(function(){
-		var url = $(this).attr("nj-url");
-		var method = $(this).attr("nj-method");
-		var object = $(this).attr("nj-object");
+		var url = $(this).attr("url");
+		var method = $(this).attr("method");
+		var object = $(this).attr("object");
 		
 		$.ajax({
 				url: url,
  				method : method,
 				success:function(response){
-					//alert(response["title"])
-					$(":attrStartsWith('nj-')").each(function() {
-						var $element = $(this);
-					  $.each(this.attributes, function() {
-					    // this.attributes is not a plain object, but an array
-					    // of attribute nodes, which contain both the name and value
-					    if(this.specified) {
-					    	if(this.value.match("^"+object)){
-					   			$element.text("attrStartsWith"); 		
-					    	}
-					    }
-					  });
-					});
-					//$(":attrStartsWith('nj-')").find("[nj-text^="+object+"]").text("attrStartsWith");
-					//$("[nj-text^="+object+"]").text("test");
+					//We add result to the vars array
+					//var r =response;
+					eval("window."+object+"=response");
+					//For each element having 
+					
 				}
   		  });
 		
 
 		
 	});
+	$(":attrStartsWith('nj-')").each(function() {
+		var $element = $(this);
+		$.each(this.attributes, function() {
+	  	if(this.specified) {
+	    	if(this.name.match("^nj-")){
+	    		evaluateExpression($element, this.name, this.value);
+	   					
+	    	}
+	    }
+	  });
+	});
+});
+function evaluateExpression(element,attribute, expression){
+	switch (attribute) {
+		case "nj-text":
+			evaluateText(element, expression);
+			break;
+		case "nj-if":
+			evaluateIf(element, expression);
+			break;
+		case "nj-class":
+			evaluateClass(element, expression);
+			break;
+		case "nj-each":
+			evaluateEach(element,expression);
+			break;
+
+	}
+}
+function evaluateText(element,expression){
+	var text ;
+	try{
+		text = eval(expression);
+		element.text(text);
+	}
+	catch(e){
+		alert("Error occured");
+	}
 	
-})
+}
+function evaluateIf(element,expression){
+	try{
+		var result = eval(expression);
+		if(!result){
+			element.remove();
+		}
+	}
+	catch(e){
+		alert("Error occured");
+	}
+
+}
+function evaluateClass(element,expression){
+	try{
+		var result = eval(expression);
+		element.attr("class",result);
+	}
+	catch(e){
+		alert("Error occured");
+	}
+
+}
+function evaluateEach(__element, expression){
+	var variable = expression.split(":")[0].trim();
+	var listName = expression.split(":")[1].trim();
+	var __list = eval(listName);
+	var $__parent = __element.parent();
+	$__parent.html("");
+	__element.removeAttr("nj-each");
+	for(var i=0; i<__list.length;i++){
+		$__newElement = __element.clone();
+		eval("window."+variable+"=__list["+i+"]");
+		$__parent.append($__newElement);
+		$__newElement.each(function() {
+		$.each(this.attributes, function() {
+	  	if(this.specified) {
+	    	if(this.name.match("^nj-")){
+	    		evaluateExpression($__newElement, this.name, this.value);
+	   					
+	    	}
+	    }
+	  });
+	});
+		
+		
+	}
+	//alert(listName);
+	//alert(eval(variable));
+
+}
